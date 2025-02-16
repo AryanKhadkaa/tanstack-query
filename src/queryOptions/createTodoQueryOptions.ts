0ac339e7a9c959d5bfe09c1eeedd7402d46944a9
AryanKhakdaa <khadkaaryan809@gmail.com>
2025-02-16 18:31:55 +0545

    cleaned up some code

--- src/queryOptions/createTodoQueryOptions.ts
+++ src/queryOptions/createTodoQueryOptions.ts
@@ -5,22 +5,22 @@
 
 import { queryOptions } from "@tanstack/react-query";
 
-export function createTodoQueryOptions(postId:number,on:boolean) {
+export function createTodoQueryOptions() {
 
     return queryOptions({
          
-    queryKey: ["todos", postId], // queryKey is an array and must be unique, as different quieries cannot have same keys to identify them
-    queryFn: () => getTodos(postId), //we're not calling the fucnction there so we dont need the paranthesis[()], we're just passing the function
+    queryKey: ["todos"], // queryKey is an array and must be unique, as different quieries cannot have same keys to identify them
+    queryFn: () => getTodos, //we're not calling the fucnction there so we dont need the paranthesis[()], we're just passing the function
 
-    enabled: on,
+    enabled: true,
         //This is applicable in situations when we have multuple useQueries in the same compoenent and we only need certain ones depending on some states on your compoenent
     })    
 }
 
-const getTodos = async (postId: number) => {
+const getTodos = async () => {
   // const response = await fetch("https://jsonplaceholder.typicode.com/todos"); //using a mock api
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+    `https://jsonplaceholder.typicode.com/comments?postId=${1}`
   );
   return await response.json(); //returns the parsed json data when called
 };
