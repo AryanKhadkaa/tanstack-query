b3096860049e68f1ceb52f4865492bf598fd966c
AryanKhakdaa <khadkaaryan809@gmail.com>
2025-02-16 18:15:54 +0545

    updated comments

--- src/queryOptions/createTodoQueryOptions.ts
+++ src/queryOptions/createTodoQueryOptions.ts
@@ -1,6 +1,9 @@
+// As out useQuery hook can we long and bulky and a little lengthy which may not be pleassing to see,
+// we can rather create a different file and create a function in it to handle querying and resue it in other compoenents
+
+// this will make it reusable and well managed
+
 import { queryOptions } from "@tanstack/react-query";
-
-
 
 export function createTodoQueryOptions(postId:number,on:boolean) {
 
@@ -8,7 +11,6 @@ export function createTodoQueryOptions(postId:number,on:boolean) {
          
     queryKey: ["todos", postId], // queryKey is an array and must be unique, as different quieries cannot have same keys to identify them
     queryFn: () => getTodos(postId), //we're not calling the fucnction there so we dont need the paranthesis[()], we're just passing the function
-
 
     enabled: on,
         //This is applicable in situations when we have multuple useQueries in the same compoenent and we only need certain ones depending on some states on your compoenent
